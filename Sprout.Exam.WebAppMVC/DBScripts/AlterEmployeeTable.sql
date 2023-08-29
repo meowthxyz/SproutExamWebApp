5f33cd9702a83cf89992a10c63c08406857339b0
Matt Ignacio <dev2.matt@gmail.com>
2023-08-30 00:47:00 +0800

    Added alter script for employee table: [FK_Employee_EmployeeType]

---
+++ Sprout.Exam.WebAppMVC/DBScripts/AlterEmployeeTable.sql
@@ -0,0 +1,6 @@
+﻿ALTER TABLE [dbo].[Employee]  WITH CHECK ADD  CONSTRAINT [FK_Employee_EmployeeType] FOREIGN KEY([EmployeeTypeId])
+REFERENCES [dbo].[EmployeeType] ([Id])
+GO
+
+ALTER TABLE [dbo].[Employee] CHECK CONSTRAINT [FK_Employee_EmployeeType]
+GO
